2dde8e851996a4bbe72670cae4936621ed32ee8e
Daniel Henrique Lelis de Almeida <daniel111906@gmail.com>
2020-08-19 16:41:04 -0300

    Move over

--- competicao_am/metodo_competicao.py
+++ competicao_am/metodo_competicao.py
@@ -1,6 +1,6 @@
 from base_am.metodo import MetodoAprendizadoDeMaquina
 import pandas as pd
-from .preprocessamento_atributos_competicao import gerar_atributos_ator, gerar_atributos_resumo
+from .preprocessamento_atributos_competicao import DataframePreprocessing
 from base_am.resultado import Resultado
 from typing import Union, List
 from sklearn.base import ClassifierMixin, RegressorMixin
@@ -9,92 +9,122 @@ from sklearn.svm import LinearSVC
 
 
 class MetodoCompeticao(MetodoAprendizadoDeMaquina):
-    def __init__(self, ml_method: Union[ClassifierMixin, RegressorMixin]):
+    def __init__(self, ml_method: Union[ClassifierMixin, RegressorMixin], df_treino: pd.DataFrame, df_data_to_predict: pd.DataFrame, col_classe: str):
         self.ml_method = ml_method
 
-        self.dic_int_to_nom_classe = {}
-        self.dic_nom_classe_to_int = {}
-
-    def class_to_number(self, y):
-        arr_int_y = []
-
-        for rotulo_classe in y:
-            if rotulo_classe not in self.dic_nom_classe_to_int:
-                int_new_val_classe = len(self.dic_nom_classe_to_int.keys())
-                self.dic_nom_classe_to_int[rotulo_classe] = int_new_val_classe
-                self.dic_int_to_nom_classe[int_new_val_classe] = rotulo_classe
-
-            arr_int_y.append(self.dic_nom_classe_to_int[rotulo_classe])
-
-        return arr_int_y
-
-    def obtem_y(self, df_treino: pd.DataFrame, df_data_to_predict: pd.DataFrame, col_classe: str):
-        y_treino = self.class_to_number(df_treino[col_classe])
-        y_to_predict = None
-
-        if col_classe in df_data_to_predict.columns:
-            y_to_predict = self.class_to_number(df_data_to_predict[col_classe])
-
-        return y_treino, y_to_predict
-
-    def obtem_x(self, df_treino: pd.DataFrame, df_data_to_predict: pd.DataFrame, col_classe: str):
-        x_treino = df_treino.drop(col_classe, axis=1)
-        x_to_predict = df_data_to_predict
-        if col_classe in df_data_to_predict.columns:
-            x_to_predict = df_data_to_predict.drop(col_classe, axis=1)
-        return x_treino, x_to_predict
-
-    def eval_actors(self, df_treino: pd.DataFrame, df_data_to_predict: pd.DataFrame, col_classe: str, seed: int = 1):
-        x_treino, x_to_predict = self.obtem_x(
-            df_treino, df_data_to_predict, col_classe)
-        y_treino, y_to_predict = self.obtem_y(
+        self.preprocessor = DataframePreprocessing(
             df_treino, df_data_to_predict, col_classe)
 
-        df_treino_ator, df_to_predict_ator = gerar_atributos_ator(
-            x_treino, x_to_predict)
+    def __init__(self, ml_method: Union[ClassifierMixin, RegressorMixin], preprocessor: DataframePreprocessing):
+        self.ml_method = ml_method
 
-        arr_df_to_remove_id = [df_treino_ator, df_to_predict_ator]
-        for df_data in arr_df_to_remove_id:
-            df_data.drop("id", axis=1)
+        self.preprocessor = preprocessor
 
-        self.ml_method.fit(df_treino_ator, y_treino)
-        arr_predict = self.ml_method.predict(df_to_predict_ator)
+    def __init__(self, ml_method: Union[ClassifierMixin, RegressorMixin]):
+        self.ml_method = ml_method
+        self.preprocessor = None
+
+    # Properties
+
+    #! Might remove
+
+    @property
+    def df_treino(self):
+        return self.preprocessor.df_treino
+
+    @property
+    def df_data_to_predict(self):
+        return self.preprocessor.df_data_to_predict
+
+    @property
+    def df_data_to_predict(self):
+        return self.preprocessor.col_classe
+
+    @property
+    def x(self):
+        return self.preprocessor.x
+
+    @property
+    def y(self):
+        return self.preprocessor.y
+
+    #! End
+
+    # Combiner
+
+    def combine_predictions(self, *predictions: Tuple[List[int]]) -> List[int]:
+        y_final_predictions = []
+
+        if len(predictions) < 1:
+            raise ValueError("Predictins can't be empty")
+
+        for i in len(predictions[0]):
+            comedy = 0
+            action = 0
+
+            for prediction in predictions:
+                if self.preprocessor.nom_classe(prediction[i]) == 'Comedy':
+                    comedy += 1
+                else:
+                    action += 1
+
+            if comedy > action:
+                y_final_predictions.append(
+                    self.preprocessor.int_classe('Comedy'))
+            else if comedy == action:
+                y_final_predictions.append(predictions[0][i])
+            else:
+                y_final_predictions.append(
+                    self.preprocessor.int_classe('Action'))
+
+        return y_final_predictions
+
+    # Evals
+
+    def _eval_boi(self, items: List[str], min_occur: int = 3, seed: int = 1):
+        y_treino, y_to_predict = self.y
+
+        df_treino_boi, df_to_predict_boi = self.preprocessor.bag_of_items(
+            items, min_occur)
+        df_treino_boi, df_to_predict_boi = DataframePreprocessing.remove_id(
+            df_treino_boi, df_to_predict_boi)
+
+        # ? Should the seed go here?
+        self.ml_method.fit(df_treino_boi, y_treino)
+        arr_predict = self.ml_method.predict(df_to_predict_boi)
 
         return y_to_predict, arr_predict
 
-    def eval_bow(self, df_treino: pd.DataFrame, df_data_to_predict: pd.DataFrame, col_classe: str, seed: int = 1):
-        x_treino, x_to_predict = self.obtem_x(
-            df_treino, df_data_to_predict, col_classe)
-        y_treino, y_to_predict = self.obtem_y(
-            df_treino, df_data_to_predict, col_classe)
+    def _eval_bow(self, column: str, seed: int = 1):
+        y_treino, y_to_predict = self.y
 
-        df_treino_bow, df_to_predict_bow = gerar_atributos_resumo(
-            x_treino, x_to_predict)
+        df_treino_bow, df_to_predict_bow = self.preprocessor.bag_of_words(
+            column)
+        df_treino_bow, df_to_predict_bow = DataframePreprocessing.remove_id(
+            df_treino_boi, df_to_predict_boi)
 
+        # ? Should the seed go here?
         self.ml_method.fit(df_treino_bow, y_treino)
         arr_predict = self.ml_method.predict(df_to_predict_bow)
 
         return y_to_predict, arr_predict
 
-    def combine_predictions(self, arr_predictions_1: List[int], arr_predictions_2: List[int]) -> List[int]:
+    def eval(self, seed: int = 1) -> Resultado:
 
-        y_final_predictions = []
-        for i, pred in enumerate(arr_predictions_1):
-            if self.dic_int_to_nom_classe[pred] == 'Comedy':
-                y_final_predictions.append(pred)
-            else:
-                y_final_predictions.append(arr_predictions_2[i])
-        return y_final_predictions
+        y_to_predict, predictions_ator = self._eval_boi(
+            ["ator_1", "ator_2", "ator_3", "ator_4", "ator_5"])
+        y_to_predict, predictions_escritor = self._eval_boi(
+            ["escrito_por_1", "escrito_por_2"])  # Novo
+        y_to_predict, predictions_resumo = self._eval_bow("resumo")
+        y_to_predict, predictions_titulo = self._eval_bow("titulo")  # Novo
 
-    def eval(self, df_treino: pd.DataFrame, df_data_to_predict: pd.DataFrame, col_classe: str, seed: int = 1):
-
-        y_to_predict, arr_predictions_ator = self.eval_actors(
-            df_treino, df_data_to_predict, col_classe)
-
-        y_to_predict, arr_predictions_bow = self.eval_bow(
-            df_treino, df_data_to_predict, col_classe)
-
-        arr_final_predictions = self.combine_predictions(
-            arr_predictions_ator, arr_predictions_bow)
+        final_predictions = self.combine_predictions(
+            predictions_resumo, predictions_titulo, predictions_escritor, predictions_ator)  # Ordem indica prioridade
 
         return Resultado(y_to_predict, arr_final_predictions)
+
+    def eval(self, df_treino: pd.DataFrame, df_data_to_predict: pd.DataFrame, col_classe: str, seed: int = 1) -> Resultado:
+        self.preprocessor = DataframePreprocessing(
+            df_treino, df_data_to_predict, col_classe)
+
+        return eval(seed=seed)
--- competicao_am/preprocessamento_atributos_competicao.py
+++ competicao_am/preprocessamento_atributos_competicao.py
@@ -4,40 +4,109 @@ from typing import List, Tuple
 from base_am.preprocessamento_atributos import BagOfWords, BagOfItems
 
 
-def gerar_atributos_ator(df_treino: pd.DataFrame, df_data_to_predict: pd.DataFrame) -> pd.DataFrame:
-    obj_bag_of_actors = BagOfItems(min_occur=3)
-    df_treino_boa = obj_bag_of_actors.cria_bag_of_items(
-        df_treino, ["ator_1", "ator_2", "ator_3", "ator_4", "ator_5"])
-    df_data_to_predict_boa = obj_bag_of_actors.aplica_bag_of_items(
-        df_data_to_predict, ["ator_1", "ator_2", "ator_3", "ator_4", "ator_5"])
-
-    return df_treino_boa, df_data_to_predict_boa
-
-
-def gerar_atributos_resumo(df_treino: pd.DataFrame, df_data_to_predict: pd.DataFrame) -> pd.DataFrame:
-    bow_amostra = BagOfWords()
-    df_bow_treino = bow_amostra.cria_bow(df_treino, "resumo")
-    df_bow_data_to_predict = bow_amostra.aplica_bow(
-        df_data_to_predict, "resumo")
-
-    return df_bow_treino, df_bow_data_to_predict
-
-
 class DataframePreprocessing:
-    def __init__(self, df_treino: pd.DataFrame, df_data_to_predict: pd.DataFrame):
-        self.df_treino = df_treino
-        self.df_data_to_predict = df_data_to_predict
+    def __init__(self, df_treino: pd.DataFrame, df_data_to_predict: pd.DataFrame, col_classe: str):
+        # Attributes
+        self._df_treino = df_treino.copy()
+        self._df_data_to_predict = df_data_to_predict.copy()
+        self._col_classe = col_classe
 
-    def _bag_of_items(self, items: List[str], min_occur: int = 3) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        # Class to number
+        self.dic_int_to_nom_classe = {}
+        self.dic_nom_classe_to_int = {}
+
+        # Processing
+        self._process()
+
+    @staticmethod
+    def remove_id(*args: Tuple[pd.DataFrame]):
+        dfs = (df.drop(columns='id') for df in args)
+        return dfs[0] if len(dfs) == 1 else dfs
+
+    def _process(self):
+        """
+        Aplica operações globais ao dataframe
+        """
+        pass
+        # self._df_treino = self._df_treino.drop(columns=['id'])
+        # self._df_data_to_predict = self._df_treino.drop(columns=['id'])
+
+    def class_to_number(self, y):
+        """
+        Transforma classes em um mapa númerico
+        Os mapeamentos são salvos em dic_int_to_nom_classe e dic_nom_classe_to_int
+        """
+        arr_int_y = []
+
+        for rotulo_classe in y:
+            if rotulo_classe not in self.dic_nom_classe_to_int:
+                int_new_val_classe = len(self.dic_nom_classe_to_int.keys())
+                self.dic_nom_classe_to_int[rotulo_classe] = int_new_val_classe
+                self.dic_int_to_nom_classe[int_new_val_classe] = rotulo_classe
+
+            arr_int_y.append(self.dic_nom_classe_to_int[rotulo_classe])
+
+        return arr_int_y
+
+    @property
+    def df_treino(self) -> pd.DataFrame:
+        return self._df_treino
+
+    @property
+    def df_data_to_predict(self) -> pd.DataFrame:
+        return self._df_data_to_predict
+
+    @property
+    def col_classe(self) -> str:
+        return self._col_clase
+
+    @property
+    def x(self):
+        x_treino = self.df_treino.drop(self.col_classe, axis=1)
+        x_to_predict = self.df_data_to_predict
+
+        if self.col_classe in self.df_data_to_predict.columns:
+            x_to_predict = self.df_data_to_predict.drop(
+                self.col_classe, axis=1)
+
+        return x_treino, x_to_predict
+
+    @property
+    def y(self):
+        y_treino = self.class_to_number(self.df_treino[self.col_classe])
+        y_to_predict = None
+
+        if self.col_classe in self.df_data_to_predict.columns:
+            y_to_predict = self.class_to_number(
+                self.df_data_to_predict[self.col_classe])
+
+        return y_treino, y_to_predict
+
+    def nom_classe(self, classe: int) -> str:
+        return self.dic_int_to_nom_classe[classe]
+
+    def int_classe(self, classe: str) -> int:
+        return self.dic_nom_classe_to_int[classe]
+
+    def bag_of_items(self, items: List[str], min_occur: int = 3) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        """
+        Cria uma bag of items a partir dos items passados
+        """
+
+        x_treino, x_to_predict = self.x
+
         boi = BagOfItems(min_occur=min_occur)
 
-        training_boi_df = boi.cria_bag_of_items(self.df_treino, items)
+        training_boi_df = boi.cria_bag_of_items(x_treino, items)
         predict_boi_df = boi.aplica_bag_of_items(
-            self.df_data_to_predict, items)
+            x_to_predict, items)
 
         return training_boi_df, predict_boi_df
 
-    def _bag_of_words(self, column: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    def bag_of_words(self, column: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        """
+        Cria uma bag of words a partir da coluna passada
+        """
         bow = BagOfWords()
 
         training_bow_df = bow.cria_bow(self.df_treino, column)
